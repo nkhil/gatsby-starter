992523d57eee1f513b0510aec31eea1bd97bb972
Nikhil Vijayan <nkhilv@gmail.com>
2019-08-16 20:52:27 +0100

    feat: add some pages

---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+export default function contact() {
+  return (
+    <>
+      <Link to='/'>Home</Link>
+      <p>This is the contact page</p>
+    </>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
-import React from "react"
+import React from 'react';
+import { Link } from 'gatsby';
 
-export default () => <div>Hello world!</div>
+// export default () => <div>Hello world!</div>
+
+const home = () => {
+  return (
+    <>
+      <Link to='/contact'>Contact</Link>
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default home
